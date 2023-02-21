467246048b06ec062dbce87dee37d964a0e5cfd3
trinidad cerri <trinidad.cerri@gmail.com>
2023-02-21 10:44:38 +0100

    Rails Task Manager without style

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  # before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   # I can list tasks
   def index
     @tasks = Task.all
@@ -7,7 +7,6 @@ class TasksController < ApplicationController
 
   # I can view the details of a task
   def show
-    @task = Task.find(params[:id])
   end
 
   # I can add a new task (new and create)
@@ -23,26 +22,24 @@ class TasksController < ApplicationController
 
   # I can edit a task (edit and update)
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
   # I can remove a task
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
+    redirect_to tasks_path, status: :see_other
   end
 
   private
 
-  # def set_task
-  #   @task = Task.find(params[:id])
-  # end
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get 'tasks', to: 'tasks#index' # I can list tasks
-  get 'tasks/:id', to: 'tasks#show' # I can view the details of a task
   get 'tasks/new', to: 'tasks#new' # I can add a new task
+  get 'tasks/:id', to: 'tasks#show' # I can view the details of a task
   post 'tasks', to: 'tasks#create' # I can add a new task
-  get 'tasks/:id/edit', to: 'tasks#edit' # I can edit a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'task_edit' # I can edit a task
   patch 'tasks/:id', to: 'tasks#update', as: 'task' # I can edit a task
   delete 'tasks/:id', to: 'tasks#destroy' # I can remove a task
   # resources :tasks, except: / only: [:create]
